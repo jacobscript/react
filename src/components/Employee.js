bf6b787888e62c939c319c354dcadfa80bf26be5
Jacob Lantsman <jacob.lantsman@gmail.com>
2023-08-02 11:57:57 -0400

    Pass component to props

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,25 @@
-function Employee(props){
-return (
-    <>
-    <h3>Employee {props.name}</h3>
-    <p>{props.role ? props.role : 'No role'}</p>
-    </>
-)
+import EditEmployee from './EditEmployee'
+
+function Employee(props) {
+    return (
+        <div className='min-w-[350px] max-w-[350px] m-2 py-8 px-8 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>
+            <img
+                className='object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0'
+                src={props.img}
+                alt="Employee's Face"
+            />
+            <div className='text-center space-y-2 sm:text-left'>
+                <div className='space-y-0.5'>
+                    <p className='text-lg text-black font-semibold'>
+                        {props.name}
+                    </p>
+                    <p className='text-slate-500 font-medium'>{props.role}</p>
+                </div>
+
+                {props.editEmployee}
+            </div>
+        </div>
+    )
 }
 
 export default Employee
